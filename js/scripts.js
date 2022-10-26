d9fde60d388a08cb55c09b88058144bcb999d932
Yevhenia Nikolska <mac@MacBook-Pro-mac.local>
2022-10-26 15:24:05 +0300

    start

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,17 @@
 
+const numberOfFilms = +prompt('How much films have you already watched?', '')
+const lastSeenFilm = prompt('One of the latest seen films?', '')
+const lastFilmRating = +prompt('How would you feedback it?', '')
+const lastSeenFilm1 = prompt('One of the latest seen films?', '')
+const lastFilmRating1 = +prompt('How would you feedback it?', '')
 
-const answers = []
-// answers[0] = prompt('Your name', '')
-// answers[1] = prompt('Your surname', '')
-// answers[2] = prompt('Your age', '')
-
-console.log(typeof (answers))
+const personalMovieDB ={
+  count: numberOfFilms,
+  movies: {},
+  actors: {},
+  genres: [],
+  privat: false,
+}
+personalMovieDB.movies[lastSeenFilm] = lastFilmRating
+personalMovieDB.movies[lastSeenFilm1] = lastFilmRating1
+console.log(personalMovieDB)
