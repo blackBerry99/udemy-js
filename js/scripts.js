47afd26ca7f8f09f927ab82d1e2a1ba5cdd47f99
me <yevheniaterletska@gmail.com>
2022-10-28 19:19:43 +0300

    cycles/conditions

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,82 @@
-const hamburger = 3;
-const fries = 3;
-const cola = 0;
-const nuggets = 2;
+// Место для первой задачи
+function firstTask() {
+  // Пишем решение вот тут
+  for (let i = 5; i <= 10; i +=1){
+    console.log(i)
+  }
 
-if (hamburger === 3 && cola || fries === 3 && nuggets) {
-  console.log('Done!')
 }
 
+// Место для второй задачи
+function secondTask() {
+  // Пишем решение вот тут
+  for (let i = 20; i >= 10; i -=1){
+    console.log(i);
+    if (i === 13) {
+      break;
+    }
+  }
+
+}
+
+// Место для третьей задачи
+function thirdTask() {
+  // Пишем решение вот тут
+  for (let i = 2; i <= 10; i += 1){
+    if (i%2 === 0) {
+      console.log(i);
+    }
+  }
+
+}
+
+// Место для четвертой задачи
+
+// Цикл, который нужно переписать:
+
+// for (let i = 2; i <= 16; i++) {
+//     if (i % 2 === 0) {
+//         continue;
+//     } else {
+//         console.log(i);
+//     }
+// }
+
+function fourthTask() {
+  // Пишем решение вот тут
+  let i = 2;
+  while (i <= 16) {
+    if (i % 2 !== 0){
+      console.log(i);
+    }
+    i +=1;
+  }
+
+}
+
+// Место для пятой задачи
+
+function fifthTask() {
+  const arrayOfNumbers = [];
+
+  first: for (let i = 5; i <= 10; i += 1){
+    for (let j = i - 5; j <= 5; j += 1){
+      arrayOfNumbers[j] = i;
+      continue first;
+    }
+  }
+  return arrayOfNumbers;
+}
+
+
+function firstTask() {
+  // Значения массива менять нельзя, тут он проверяется автоматически именно на эти значения
+  const arr = [3, 5, 8, 16, 20, 23, 50];
+  const result = [];
+
+  // Пишем решение вот тут
+
+
+  // Не трогаем
+  return result;
+}
